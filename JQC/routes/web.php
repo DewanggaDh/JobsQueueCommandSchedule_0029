4d28a10a93d941aaf3a16aee82401a8dee994d83
DewanggaDh <73766205+DewanggaDh@users.noreply.github.com>
2022-04-26 15:01:41 +0700

    meh

--- JQC/routes/web.php
+++ JQC/routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('testingJob',function(){
-    dispatch(new App\Jobs\JobSingkat());
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
